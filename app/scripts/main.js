d7fa3aa3e830fc1ce3a78a99afc2bfc3ed92c419
Luke Coy <coy.luke@gmail.com>
2015-09-22 13:12:42 -0400

    Move everything to gulp and abondon ye old grunt

---
+++ app/scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var $allContent;
+  $allContent = $('.pusher .all-content');
+  $('.bottom.demo.sidebar').sidebar('setting', {
+    dimPage: false,
+    transition: 'overlay',
+    mobileTransition: 'overlay'
+  });
+  return $('.button.links').on('click', function() {
+    if ($(this).filter('.disabled').size() === 0) {
+      return $('.bottom.demo.sidebar').sidebar('toggle');
+    }
+  });
+});
